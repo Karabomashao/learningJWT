a3d449cba2fade59bce7821144d8573cfe2df787
kmashao <karabomashao1@gmail.com>
2025-01-30 02:05:57 +0200

    Configuring application.yml to connect to database

---
+++ src/main/java/com/example/demo/user/UserData.java
@@ -0,0 +1,10 @@
+package com.example.demo.user;
+
+public class UserData {
+
+    private int userId;
+    private String fistName;
+    private String lastName;
+    private String email;
+    private String password;
+}
